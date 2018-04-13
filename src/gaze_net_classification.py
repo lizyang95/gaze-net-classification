340f0c1de52abb06dfeb089a3d7e1f0d590d0a30
lizyang95 <yy4@cs.cmu.edu>
2018-04-12 23:33:00 -0400

    main function done

--- src/gaze_net_classification.py
+++ src/gaze_net_classification.py
@@ -21,9 +21,11 @@ from logger import Logger
 import gazenetGenerator as gaze_gen
 
 def main():
+    TARIN = True
     num_class = 6
-    batch_size = 1
-    time_step = 32
+    batch_size = 4
+    gaze_gen_batch_size = 1
+    gaze_gen_time_step = 32
     epochs = 50
     cnn_feat_size = 256     # AlexNet
     gaze_size = 3
@@ -32,8 +34,8 @@ def main():
     weight_decay = 1e-4
     eval_freq = 1       # epoch
     print_freq = 1      # iteration
-    dataset_path = '../../gaze-net/gaze_dataset_classification'
-    img_size = 128
+    dataset_path = '../../gaze-net/gaze_dataset_old'
+    img_size = (224,224)
     log_path = '../log'
     logger = Logger(log_path, 'classification')
 
@@ -45,23 +47,89 @@ def main():
     # define generator
     trainGenerator = gaze_gen.GazeDataGenerator(validation_split=0.2)
     train_data = trainGenerator.flow_from_directory(dataset_path, subset='training', crop=False,
-                                batch_size=batch_size, crop_with_gaze=True,
-                                crop_with_gaze_size=img_size,class_mode='categorical')
+                                batch_size=gaze_gen_batch_size, crop_with_gaze=True,time_steps=gaze_gen_time_steps,
+                                crop_with_gaze_size=img_size[0],class_mode='categorical')
     # small dataset, error using validation split
     val_data = trainGenerator.flow_from_directory(dataset_path, subset='validation', crop=False,
-                                batch_size=batch_size, crop_with_gaze=True,
-                                crop_with_gaze_size=img_size, class_mode='categorical')
-    def test(train_data):
-        img_seq, target = next(train_data)
-        img = img_seq[0,1,:,:,:]
-        plt.imshow(img)
-        plt.show()
-    test(train_data)
+                                batch_size=gaze_gen_batch_size, crop_with_gaze=True,time_steps=gaze_gen_time_steps,
+                                crop_with_gaze_size=img_size[0], class_mode='categorical')
+
+    para = {'bs': batch_size, 'img_size': img_size, 'num_class': num_class,
+            'print_freq': print_freq}
     print("finished here")
 
+    if Train:
+        print("training mode")
+        best_acc = 0
+        for epoch in range(epochs):
+            adjust_learning_rate(optimizer,epoch,learning_rate)
+            print('Epoch: {}'.format(epoch))
+            train(train_data, model, criterion, optimizer, epoch, logger, para)
+            if epoch % eval_freq ==0 or epoch == epochs-1:
+                acc = validate(val_data,model,criterion,optimizer,epoch,logger,para)
+                is_best = acc>best_acc
+                save_checkpoint({
+                    'epoch': epoch + 1,
+                    'arch': arch,
+                    'state_dict': model.state_dict(),
+                    'best_acc': best_acc,
+                    'optimizer': optimizer.state_dict(),
+                }, is_best)
+    else:
+        print("let's test the model")
+        model = load_checkpoint(model)
+        print("get input test and visualize mode")
+        print("visualizing the training data")
+
+        vis_data_path = '../vis/train'
+        if not os.path.exists(vis_data_path):
+            os.makedirs(vis_data_path)
+        acc = validate(train_data, model, criterion, -1, \
+                        logger, para, True, vis_data_path)
+        print("visualization for validation data")
+        vis_data_path = '../vis/val/'
+        if not os.path.exists(vis_data_path):
+            os.makedirs(vis_data_path)
+        acc = validate(val_data, model, criterion, -1, \
+                        logger, para, True, vis_data_path)
+def train(train_data,model,criterion,optimizer,epoch,logger,para):
+    bs = para['bs']
+    img_size = para['img_size']
+    num_class = para['num_class']
+    print_freq = para['print_freq']
+
+    model.train()
+    print("training interaction number")
+    train_num = len(train_data)
+    end = time.time()
+
+    for i in range(train_num):
+        img_seq,target_seq = next(train_data)
+        img_seq = normalize(img_seq)
+        print(img_seq.shape)
 
 
+def adjust_learning_rate(optimizer, epoch, learning_rate):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = learning_rate * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
+def save_checkpoint(state, is_best, filename='../model/spatial/checkpoint.pth.tar'):
+    print("save checkpoint")
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, '../model/spatial/model_best.pth.tar')
+
+def load_checkpoint(model, filename='../model/spatial/checkpoint.pth.32.tar'):
+    if os.path.isfile(filename):
+            checkpoint = torch.load(filename)
+            epoch = checkpoint['epoch']
+            best_acc = checkpoint['best_acc']
+            model.load_state_dict(checkpoint['state_dict'])
+            print("loaded checkpoint '{}' (epoch {})"
+                  .format(filename, checkpoint['epoch']))
+    return model
 
 if __name__ == '__main__':
     main()
