f45019c33e38fc8cfa748c1a9f67759e4786c21a
lizyang95 <yy4@cs.cmu.edu>
2018-04-13 00:53:51 -0400

    printing model

--- src/gaze_net_classification.py
+++ src/gaze_net_classification.py
@@ -25,8 +25,8 @@ def main():
     num_class = 6
     batch_size = 4
     gaze_gen_batch_size = 1
-    gaze_gen_time_steps = 2
-    epochs = 2
+    gaze_gen_time_steps = 6
+    epochs = 1
     cnn_feat_size = 256     # AlexNet
     gaze_size = 3
     learning_rate = 0.0001
@@ -40,6 +40,7 @@ def main():
     logger = Logger(log_path, 'classification')
 
     model = models.alexnet(pretrained=True).cuda()
+    print(model)
     criterion = nn.CrossEntropyLoss().cuda()
 
     optimizer = torch.optim.SGD(model.parameters(), learning_rate,
@@ -54,17 +55,17 @@ def main():
                                 batch_size=gaze_gen_batch_size, crop_with_gaze=True,time_steps=gaze_gen_time_steps,
                                 crop_with_gaze_size=img_size[0], class_mode='categorical')
 
-    # para = {'bs': batch_size, 'img_size': img_size, 'num_class': num_class,
-    #         'print_freq': print_freq}
+    para = {'bs': batch_size, 'img_size': img_size, 'num_class': num_class,
+            'print_freq': print_freq}
 
-    # if TRAIN:
-    #     print("training mode")
-    #     best_acc = 0
-    #     for epoch in range(epochs):
-    #         print(learning_rate)
-    #         adjust_learning_rate(optimizer,epoch,learning_rate)
-    #         print('Epoch: {}'.format(epoch))
-            # train(train_data, model, criterion, optimizer, epoch, logger, para)
+    if TRAIN:
+        print("training mode")
+        best_acc = 0
+        for epoch in range(epochs):
+            print(learning_rate)
+            adjust_learning_rate(optimizer,epoch,learning_rate)
+            print('Epoch: {}'.format(epoch))
+            train(train_data, model, criterion, optimizer, epoch, logger, para)
             # if epoch % eval_freq ==0 or epoch == epochs-1:
             #     acc = validate(val_data,model,criterion,optimizer,epoch,logger,para)
             #     is_best = acc>best_acc
@@ -95,45 +96,55 @@ def main():
     print("finished here")
 
 
-# def train(train_data,model,criterion,optimizer,epoch,logger,para):
-#     bs = para['bs']
-#     img_size = para['img_size']
-#     num_class = para['num_class']
-#     print_freq = para['print_freq']
+def train(train_data,model,criterion,optimizer,epoch,logger,para):
+    bs = para['bs']
+    img_size = para['img_size']
+    num_class = para['num_class']
+    print_freq = para['print_freq']
+
+    model.train()
+    print("training interaction number")
+    train_num = len(train_data)
+    # print(train_num)
+    # end = time.time()
+
+    for i in range(train_num):
+        data_time.update(time.time() - end)
+        img_seq,target_seq = next(train_data)
+        print(img_seq.shape)
+        # img_seq = normalize(img_seq)
+        # print(img_seq.shape)
+        img_seq = np.reshape(img_seq,(img_seq.shape[0]*img_seq.shape[1],3,) + img_size)
+        img_seq_var = torch.autograd.Variable(torch.Tensor(img_seq).cuda(), requires_grad=True)
+        target_seq_var = torch.autograd.Variable(torch.Tensor(target_seq).cuda()).long()
+        optimizer.zero_grad()
+        output = model(img_seq_var)
+        loss = criterion(output, target_seq_var)
+
+
 #
-#     model.train()
-#     print("training interaction number")
-#     train_num = len(train_data)
-#     print(train_num)
-#     end = time.time()
-#
-#     for i in range(train_num):
-#         img_seq,target_seq = next(train_data)
-#         # img_seq = normalize(img_seq)
-#         # print(img_seq.shape)
-#
-#
-# def adjust_learning_rate(optimizer, epoch, learning_rate):
-#     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-#     lr = learning_rate * (0.1 ** (epoch // 30))
-#     for param_group in optimizer.param_groups:
-#         param_group['lr'] = lr
-#
-# def save_checkpoint(state, is_best, filename='../model/spatial/checkpoint.pth.tar'):
-#     print("save checkpoint")
-#     torch.save(state, filename)
-#     if is_best:
-#         shutil.copyfile(filename, '../model/spatial/model_best.pth.tar')
-#
-# def load_checkpoint(model, filename='../model/spatial/checkpoint.pth.32.tar'):
-#     if os.path.isfile(filename):
-#             checkpoint = torch.load(filename)
-#             epoch = checkpoint['epoch']
-#             best_acc = checkpoint['best_acc']
-#             model.load_state_dict(checkpoint['state_dict'])
-#             print("loaded checkpoint '{}' (epoch {})"
-#                   .format(filename, checkpoint['epoch']))
-#     return model
+
+def adjust_learning_rate(optimizer, epoch, learning_rate):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = learning_rate * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+def save_checkpoint(state, is_best, filename='../model/spatial/checkpoint.pth.tar'):
+    print("save checkpoint")
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, '../model/spatial/model_best.pth.tar')
+
+def load_checkpoint(model, filename='../model/spatial/checkpoint.pth.32.tar'):
+    if os.path.isfile(filename):
+            checkpoint = torch.load(filename)
+            epoch = checkpoint['epoch']
+            best_acc = checkpoint['best_acc']
+            model.load_state_dict(checkpoint['state_dict'])
+            print("loaded checkpoint '{}' (epoch {})"
+                  .format(filename, checkpoint['epoch']))
+    return model
 
 if __name__ == '__main__':
     main()
