c8e0783f14015d3eefc6cd011af579e1c9edd7b8
tcl326 <ouyangjiahong13@gmail.com>
2018-04-13 23:48:59 -0400

    validate finished, thanks to ichengit add gaze_net_classification.py !

--- src/gaze_net_classification.py
+++ src/gaze_net_classification.py
@@ -20,6 +20,7 @@ from model import GazeClassifier
 from util import *
 from logger import Logger
 import gazenetGenerator as gaze_gen
+import pdb
 
 def main():
     TRAIN = True
@@ -35,7 +36,7 @@ def main():
     weight_decay = 1e-4
     eval_freq = 1       # epoch
     print_freq = 1      # iteration
-    dataset_path = '../data/'
+    dataset_path = '../../gaze-net/gaze_dataset_old'
     img_size = (224,224)
     log_path = '../log'
     logger = Logger(log_path, 'classification')
@@ -73,6 +74,7 @@ def main():
             if epoch % eval_freq ==0 or epoch == epochs-1:
                 acc = validate(val_data,model,criterion,optimizer,epoch,logger,para)
                 is_best = acc>best_acc
+                pdb.set_trace()
                 save_checkpoint({
                     'epoch': epoch + 1,
                     'arch': arch,
@@ -150,6 +152,65 @@ def train(train_data,model,criterion,optimizer,epoch,logger,para):
             logger.scalar_summary('train/acc', acc_frame, global_step)
 
 
+def validate(val_data,model,criterion,optimizer,epoch,logger,para):
+    print("validating")
+    bs = para['bs']
+    img_size = para['img_size']
+    num_class = para['num_class']
+    print_freq = para['print_freq']
+
+    model.eval()
+    val_num = len(val_data)
+
+    end = time.time()
+    loss_sum = 0
+    ts_all = 0
+    acc_all = 0
+    for i in range(val_num):
+        # get data, img_seq: (ts,224,224,3), gaze_seq: (ts, 3), ouput: (ts, 6)
+        img_seq,target_seq = next(val_data)
+        ts_all += target_seq.shape[0]
+        img_seq = normalize(img_seq)
+        img_seq = np.reshape(img_seq,(-1,3,) + img_size)
+        img_seq_var = torch.autograd.Variable(torch.Tensor(img_seq).cuda(), requires_grad=True)
+        target_seq_var = torch.autograd.Variable(torch.Tensor(target_seq).cuda()).long()
+        target_seq_var = target_seq_var.repeat(img_seq.shape[0],1)
+        # target_seq_var = target_seq_var.view(target_seq_var.size()[0])
+        values, target_seq_var = torch.max(target_seq_var,1)
+        print(img_seq_var.size())
+        output_var = model(img_seq_var)
+        loss = criterion(output_var, target_seq_var)
+        loss_sum += loss.data[0]
+        print(loss.data)
+
+        prediction = F.softmax(output_var, dim=1)
+        acc_frame = metric_frame(output_var, target_seq_var)
+        acc_all += acc_frame
+        time_cnt = time.time() - end
+        end = time.time()
+
+        acc_cur = acc_frame / float( output_var.size()[0])
+        print('Epoch: [{0}]\t'
+              'Loss {loss:.4f}\t'
+              'Accuracy {acc:.4f}\t'.format(
+               epoch, i, val_num, time_cnt=time_cnt, loss=loss.data[0], acc=acc_cur))
+
+        # visualize
+        # if visualize and acc_cur < 0.5:
+        #     visualization(i, acc_cur, img_seq, gaze_seq, target_seq_var, prediction, vis_data_path)
+
+    loss_avg = loss_sum / float(val_num)
+    acc_avg = acc_all / float(ts_all)
+
+    print('Epoch: [{0}]\t'
+          'Loss {loss:.4f}\t'
+          'Accuracy {acc:.4f}\t'.format(
+           epoch, i, val_num, time_cnt=time_cnt, loss=loss_avg, acc=acc_avg))
+    logger.scalar_summary('val/loss', loss_avg, epoch)
+    logger.scalar_summary('val/acc', acc_avg, epoch)
+    return acc_avg
+
+
 
 
 def adjust_learning_rate(optimizer, epoch, learning_rate):
@@ -158,13 +219,13 @@ def adjust_learning_rate(optimizer, epoch, learning_rate):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def save_checkpoint(state, is_best, filename='../model/spatial/checkpoint.pth.tar'):
+def save_checkpoint(state, is_best, filename='../model/classification/checkpoint.pth.tar'):
     print("save checkpoint")
     torch.save(state, filename)
     if is_best:
-        shutil.copyfile(filename, '../model/spatial/model_best.pth.tar')
+        shutil.copyfile(filename, '../model/classification/model_best.pth.tar')
 
-def load_checkpoint(model, filename='../model/spatial/checkpoint.pth.32.tar'):
+def load_checkpoint(model, filename='../model/classification/checkpoint.pth.32.tar'):
     if os.path.isfile(filename):
             checkpoint = torch.load(filename)
             epoch = checkpoint['epoch']
